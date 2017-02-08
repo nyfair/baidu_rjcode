1ca9ba1d08c7b5aa614601d1bc9a66c9718af7c6
nyfair <nyfair2012@gmail.com>
2017-02-08 23:58:21 +0800

    Create baidu_rjcode.user.js

---
+++ baidu_rjcode.user.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name        百度云DLSite编号分析
+// @namespace   RJCode
+// @version     0.1
+// @description 分析RJ编号，提供DLSite/Doujinvoice/HVDB链接
+// @author      bdtrysb
+// @match       *://pan.baidu.com/disk/home*
+// @match       *://yun.baidu.com/disk/home*
+// @match       *://pan.baidu.com/s/*
+// @match       *://yun.baidu.com/s/*
+// @match       *://pan.baidu.com/share/link*
+// @match       *://yun.baidu.com/share/link*
+// @grant       none
+// ==/UserScript==
+
+for (var node of document.querySelectorAll('.filename')) {
+    var rj = node.title.match(/RJ\d{6}/)
+    if (rj) {
+        var djvoice = 'http://www.doujinvoice.moe/maniax/work/=/product_id/' + rj + '.html'
+        var dlsite = 'http://www.dlsite.com/maniax/work/=/product_id/' + rj + '.html'
+        var hvdb = 'http://hvdb.me/Dashboard/WorkDetails/' + Number(rj.toString().substr(2))
+        var newNode = document.createElement('span')
+        newNode.innerHTML = '<span>详情：</span><a href="' + djvoice + '" target="_blank"> Doujinvoice </a><a href="' + dlsite + '" target="_blank"> DLSite </a><a href="' + hvdb + '" target="_blank"> HVDB </a><span> | </span>'
+        node.parentNode.insertBefore(newNode, node.parentNode.childNodes[0])
+    }    
+}
