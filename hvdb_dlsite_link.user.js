dea66a04c976c75a17a3f83ba31ce17500012943
nyfair <nyfair2012@gmail.com>
2017-04-28 00:11:21 +0800

    Update hvdb_dlsite_link.user.js

--- hvdb_dlsite_link.user.js
+++ hvdb_dlsite_link.user.js
@@ -11,10 +11,9 @@
 var titleNode = document.querySelector('h2')
 var rj = titleNode.innerText.match(/\d{6}/)
 var dlsite = 'http://www.dlsite.com/maniax/work/=/product_id/RJ' + rj + '.html'
-titleNode.innerHTML = '<a href="' + dlsite + '" target="_blank">' + titleNode.innerText + '</a>' + ' RJ' + rj
+titleNode.innerHTML = '<a href="' + dlsite + '" target="_blank">' + titleNode.innerText + '</a>'
 
 var dlNode = document.querySelector('#Download')
 if (dlNode.value) {
-	dlNode.disabled = false
-	dlNode.className = "form-control"
+	titleNode.innerHTML += '<input class="form-control nav-form add-form" type="text" value=' + dlNode.value +'>'
 }
