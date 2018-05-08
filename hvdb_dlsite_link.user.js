da06d7f8447f94a52dde1723d909c0c1d9c361f2
nyfair <nyfair2012@gmail.com>
2018-05-08 16:41:25 +0800

    Update hvdb_dlsite_link.user.js

--- hvdb_dlsite_link.user.js
+++ hvdb_dlsite_link.user.js
@@ -2,7 +2,7 @@
 // @name        HVDB DLSite Link
 // @namespace   hvdb
 // @description Add DLSite Link on HVDB
-// @version     0.1
+// @version     0.2
 // @author      bdtrysb
 // @match       *://hvdb.me/Dashboard/WorkDetails/*
 // @grant       GM_setClipboard
@@ -13,16 +13,17 @@ var rj = titleNode.innerText.match(/\d{6}/)
 var dlsite = 'http://www.dlsite.com/maniax/work/=/product_id/RJ' + rj + '.html'
 titleNode.innerHTML = '<a href="' + dlsite + '" target="_blank">DLSite Links</a> RJ' + rj
 
+var paste = 'RJ' + rj
 var dlNode = document.querySelector('#Download')
 if (dlNode.value) {
   var ele = document.createElement('input')
   ele.className = 'form-control'
   ele.value = dlNode.value
   dlNode.parentNode.replaceChild(ele, dlNode.previousElementSibling)
-  
-  document.addEventListener('keydown', function(e) {
-    if (e.keyCode == 67 && e.ctrlKey) {
-      GM_setClipboard('RJ' + rj + '|' + dlNode.value)
-    }
-  })
+  paste += '|' + dlNode.value
 }
+document.addEventListener('keydown', function(e) {
+  if (e.keyCode == 67 && e.ctrlKey) {
+    GM_setClipboard(paste)
+  }
+})
