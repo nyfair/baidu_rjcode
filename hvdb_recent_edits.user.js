d9c07278747c3e04d0668882102d1901524934e7
nyfair <nyfair2012@gmail.com>
2021-01-07 18:55:49 +0800

    Update hvdb_recent_edits.user.js

--- hvdb_recent_edits.user.js
+++ hvdb_recent_edits.user.js
@@ -2,16 +2,23 @@
 // @name        HVDB Recent Edits
 // @namespace   hvdb
 // @description Open All Links on HVDB Recent Edits
-// @version     0.1
+// @version     0.2
 // @author      bdtrysb
 // @match       *://hvdb.me/Dashboard/RecentEdits/*
-// @grant       none
+// @grant       GM_setClipboard
 // ==/UserScript==
 
+var changed = []
 for (var wnode of document.querySelectorAll('.col-md-12')) {
   var txt = wnode.innerText
   var offset = txt.indexOf('Download Link')
   if ((offset > -1) && txt.substring(offset+21, offset+45) != '[Initial Creation] to []') {
     window.open(wnode.querySelector('a').href)
+    changed.push('RJ' + wnode.querySelector('a').innerText.padStart(2, '0'))
   }
 }
+document.addEventListener('keydown', function(e) {
+  if (e.keyCode == 67 && e.ctrlKey) {
+    GM_setClipboard(changed.join('|'))
+  }
+})
