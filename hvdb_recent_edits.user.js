af346c575448dc23bbbccfdeff1387e65b1ec441
nyfair <nyfair2012@gmail.com>
2021-01-24 18:44:52 +0800

    Update hvdb_recent_edits.user.js

--- hvdb_recent_edits.user.js
+++ hvdb_recent_edits.user.js
@@ -14,7 +14,7 @@ for (var wnode of document.querySelectorAll('.col-md-12')) {
   var txt = wnode.innerText
   var offset = txt.indexOf('Download Link')
   if ((offset > -1) && txt.substring(offset+21, offset+45) != '[Initial Creation] to []') {
-    var rj = 'RJ' + wnode.querySelector('a').innerText.padStart(2, '0')
+    var rj = 'RJ' + wnode.querySelector('a').innerText.padStart(6, '0')
     if (!changed.has(rj)) {
         window.open(wnode.querySelector('a').href)
         changed.add(rj)
